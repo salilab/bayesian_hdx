7d774092ac046d6ef55271385c756356b0210146
Daniel Saltzberg <saltzberg@salilab.org>
2019-05-09 16:37:52 +0200

    Tests for DHDX class

--- v2/test/test_analysis.py
+++ v2/test/test_analysis.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import unittest
 import os
 import analysis
+import numpy
 
 input_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'input'))
 
@@ -32,7 +33,7 @@ class TestParseOutputFile(unittest.TestCase):
 
         pof=analysis.ParseOutputFile(output_file) 
         self.assertEqual(len(pof.get_all_models()), 5100)     
-
+'''
 class TestOutputAnalysis(unittest.TestCase):
 
     def create_output_analysis(self):
@@ -97,6 +98,41 @@ class TestConvergence(unittest.TestCase):
 
         self.assertEqual(num_mods, n*2)
         self.assertGreater(len(pofs[0].models),len(pofs[1].models))
+'''
+
+class TestDeltaHDX(unittest.TestCase):
+
+    def test_DHDX_same_file(self):
+
+        output_file = input_dir + "/cytC_output/prod1/models_scores_sigmas-CytC_pH_6.5.dat"
+        pof1 = analysis.ParseOutputFile(output_file)
+        pof2 = analysis.ParseOutputFile(output_file)
+
+        dhdx = analysis.DeltaHDX(pof1, pof2)
+
+        diff, Z,mean1, mean2, sd1, sd2  = dhdx.calculate_dhdx()
+
+        for res in pof1.observed_residues:
+            self.assertEqual(diff[res-1],0)
+            self.assertEqual(Z[res-1],0)
+
+    def test_DHDX_different_files(self):
+
+        output_file1 = input_dir + "/cytC_output/prod1/models_scores_sigmas-CytC_pH_6.5.dat"
+        output_file2 = input_dir + "/cytC_output/prod1/models_scores_sigmas-CytC_pH_7.4.dat"
+        pof1 = analysis.ParseOutputFile(output_file1)
+        pof2 = analysis.ParseOutputFile(output_file2)
+
+        dhdx = analysis.DeltaHDX(pof1, pof2)
+
+        diff, Z, mean1, mean2, sd1, sd2 = dhdx.calculate_dhdx()
+
+        print(Z.shape)
+
+        dhdx.write_dhdx_file()
+
+        
+
 
 if __name__ == '__main__':
     unittest.main()
